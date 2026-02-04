a74265457bea8dc4ba9c7d2c3f442bbe083a00ea
Millimeter <akumakumu@gmail.com>
2026-02-04 17:47:17 +0700

    blink on _w variant

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
+#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 int main() {
-    stdio_init_all();
-    printf("Hello, mom!\n");
-    return 0;
+  stdio_init_all();
+
+  if (cyw43_arch_init()) {
+    printf("cyw43 init failed\n");
+    return -1;
+  }
+
+  while (true) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(250);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(250);
+  }
+  return 0;
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,10 @@
 ```bash
+cd pico-sdk
+
+git submodule update --init
+```
+
+```bash
 cmake -S . -B build -DPICO_BOARD=pico2_w
 
 cmake --build build --target main
