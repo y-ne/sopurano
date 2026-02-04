64938a802da257726501ad50615b144dc1170c4d
Millimeter <akumakumu@gmail.com>
2026-02-04 17:34:21 +0700

    base

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    printf("Hello, mom!\n");
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+```bash
+cmake -S . -B build -DPICO_BOARD=pico2_w
+
+cmake --build build --target main
+```
